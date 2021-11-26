f7bb245073025dc47edb7b7a3d5c41e2f038dc32
vavarine <evailson.m.barbosa@gmail.com>
2021-11-25 21:56:44 -0300

    build

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,6 @@ app.get('/', (req, res) => {
 
 app.use('/api', routes)
 
-app.listen(process.env.PORT || 3333, () => {
-	console.log(`🚀 Server started on port ${process.env.PORT || 3333}!`)
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`🚀 Server started on port ${process.env.PORT || 3000}!`)
 })
