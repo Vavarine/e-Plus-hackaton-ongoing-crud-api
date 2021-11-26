ad9be82658656cc3e4a27d94569020d547d0e96f
vavarine <evailson.m.barbosa@gmail.com>
2021-11-25 21:38:27 -0300

    build

--- dist/server.js
+++ dist/server.js
@@ -11,6 +11,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 const app = (0, _express.default)();
 app.use((0, _cors.default)());
 app.use(_express.default.json());
+app.get('/', (req, res) => {
+  return res.json({
+    message: 'aooba'
+  });
+});
 app.use('/api', _routes.default);
 app.listen(process.env.PORT || 3333, () => {
   console.log(`🚀 Server started on port ${process.env.PORT || 3333}!`);
