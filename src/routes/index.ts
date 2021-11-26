7c69fb8ff3bee9a49708b2dc62dc1edff6547bf4
vavarine <evailson.m.barbosa@gmail.com>
2021-11-26 01:07:40 -0300

    ticket

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import storesRoutes from './stores'
+import ticketRoutes from './tickets'
 
 const routes = Router()
 
 routes.use('/stores', storesRoutes)
+routes.use('/tickets', ticketRoutes)
 
 export default routes
